383deadca94b06f2ecb747cf0ca248d1aa396996
Nick Tsilivis <nick.tsilivis@chb-technologies.ch>
2018-08-02 10:54:07 +0200

    manifest try 43

--- src/App.vue
+++ src/App.vue
@@ -132,7 +132,7 @@ export default {
       hotelSNError: false,
       hotelSNCorrect: false,
       title: "Hotel Housekeeping",
-      versionNumber: "v1.1.3",
+      versionNumber: "v1.1.4",
       versionDate: "02/08/2018",
       tempHotelSN: ''
     };
